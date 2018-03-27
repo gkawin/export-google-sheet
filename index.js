959e8f1f9d1f3cb5ae2809102f85edace3d8432e
Kawin Glomjai <kawin.glomjai@axia.life>
2018-03-27 16:43:47 +0700

    set up lint

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const fs = require("fs")
 const http = require("http")
 const path = require("path")
 const csv = require("csvtojson")
+const fromPairs = require("lodash/fromPairs");
 
 const URL_SHEET = "https://docs.google.com/spreadsheets/d/1Az0Ce4KK2Yx-8LdDjgmuYJN0S6x1twjL96UNoqq8SdA/gviz/tq?usp=sharing&tqx=out:csv&gid=0";
 const ns = "translation"
 
 
 // tslint:disable-next-line:no-var-requires
-const fromPairs = require("lodash/fromPairs");
-function downloadSheet(): Promise<any> {
+function downloadSheet() {
     console.log("started download....");
     return new Promise((resolve: any, reject: any) => {
         const csvFile = path.resolve(__dirname, "data.csv");
@@ -23,24 +23,34 @@ function downloadSheet(): Promise<any> {
         });
     });
 }
-function asyncTransform(csvFile: any): Promise<any> {
+
+function asyncTransform(csvFile: any): Promise < any > {
     const m = new Map();
     return new Promise((resolve: any, reject: any) => csv({
-        trim: true,
-        ignoreEmpty: true,
-        checkType: true,
-    })
+            trim: true,
+            ignoreEmpty: true,
+            checkType: true,
+        })
         .fromFile(csvFile)
-        .on("json", (result: { _key: string, [language: string]: string }) => {
+        .on("json", (result: {
+            _key: string,
+            [language: string]: string
+        }) => {
             const key = result._key;
             const lngs = Object.keys(result).slice(1);
             for (const lng of lngs) {
                 const translated = result[lng];
                 if (m.has(lng)) {
                     const _t = m.get(lng);
-                    Object.assign(_t[ns], { [key]: translated });
+                    Object.assign(_t[ns], {
+                        [key]: translated
+                    });
                 } else {
-                    m.set(lng, { [ns]: { [key]: translated } });
+                    m.set(lng, {
+                        [ns]: {
+                            [key]: translated
+                        }
+                    });
                 }
             }
         })
@@ -50,7 +60,9 @@ function asyncTransform(csvFile: any): Promise<any> {
 }
 downloadSheet()
     .then(asyncTransform)
-    .then((result: Array<{ [language: string]: string }>) => fromPairs(result))
+    .then((result: Array < {
+        [language: string]: string
+    } > ) => fromPairs(result))
     .then(async (data: any) => {
         await fs.writeFileSync(path.resolve(__dirname, "translated-data.json"), JSON.stringify(data));
     });
